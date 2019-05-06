1d33b61cf71ef6c2edfb4cbf2608c7b3802c473f
mirman-mtaggart <mtaggart@mirman.org>
2019-05-06 12:42:08 -0700

    create volume

--- partition-disk.sh
+++ partition-disk.sh
@@ -1,10 +1,19 @@
 # So it's not actually a partition in APFS. It's a new Volume.
 
-# Create the Volume
+# Create the Volume if requested
 if [ $mds_var6 == "1" ]; then
-    VOLUME=$(diskutil | grep "Mirmantosh" | awk "{print $(NF)}")
-    echo "Creating new Volume Users at $VOLUME"
-    diskutil addVolume $VOLUME APFS "Users"
+    # Grab device id from diskutil list
+    DISK=$(diskutil list | grep "synthesized" | cut -c 1-10)
+    echo "Creating new Volume Users at $DISK"
+
+    # Create APFS Volume. No size needed
+    diskutil apfs addVolume $DISK APFS "Users"
+
+    # Move folders over to new volume
+    mv /Volumes/Mirmantosh/Users/* /Volumes/Users/
+    rm -rf /Volumes/Mirmantosh/Users
+    ln -s /Volumes/Users /Volumes/Mirmantosh/Users
+    
 else
     echo "No User Volume created"
     exit 0
