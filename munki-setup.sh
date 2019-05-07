3981b75ee87ba1d04e6ca1b22c4027e731a00caf
mirman-mtaggart <mtaggart@mirman.org>
2019-05-07 15:46:32 -0700

    quote removal

--- munki-setup.sh
+++ munki-setup.sh
@@ -5,9 +5,10 @@ MUNKI_FILE=/Library/Preferences/ManagedInstalls.plist
 # ClientIdentifier: mds_var1
 
 # Set Client Identifier
-defaults write $MUNKI_FILE ClientIdentifier "$mds_var1"
+defaults write $MUNKI_FILE ClientIdentifier $mds_var1
 
-defaults write $MUNKI_FILE SoftwareRepoURL "$mds_var7"
+# Software Repo URL
+defaults write $MUNKI_FILE SoftwareRepoURL $mds_var7
 
 # Set Install Apple Updates
 defaults write $MUNKI_FILE InstallAppleSoftwareUpdates -bool true
