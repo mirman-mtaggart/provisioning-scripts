182617699d58762aefa3e1a49d9abd6835765de4
mirman-mtaggart <mtaggart@mirman.org>
2019-05-10 06:34:31 -0700

    Wifi quotes fixed

--- join-wifi.sh
+++ join-wifi.sh
@@ -2,6 +2,6 @@
 
 SSID=$mds_var8
 PASSWORD=$mds_var9
-networksetup -addpreferredwirelessnetworkatindex en0 "'$SSID'" 0 wpa2 $PASSWORD
+networksetup -addpreferredwirelessnetworkatindex en0 "$SSID" 0 wpa2 $PASSWORD
 
 exit 0
